f6c5290cc7e84b759986bf9387648684f77144aa
Mazeto <mgmazeto@gmail.com>
2019-10-21 15:10:39 -0300

    Refactoring

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,11 +3,8 @@ import React, { useState } from 'react';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from '../../style/global';
 
-import Sidebar from '../sidebar';
 import Header from '../header';
-
 import { Wrapper } from './style';
-import Menu from '../menu';
 
 function Layout({ children }) {
   const [open, setOpen] = useState(false);
