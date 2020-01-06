7d7deadd2f472a2f935461f76f50129cdb27cf7c
Mazeto <mgmazeto@gmail.com>
2020-01-06 11:33:44 -0300

    Formatting files according .prettierrc rules

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,16 @@
-import React from "react"
-import PropTypes from "prop-types"
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Layout = ({ children }) => {
-
-
   return (
     <>
       <main>{children}</main>
     </>
-  )
-}
+  );
+};
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-}
+};
 
-export default Layout
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
-import React from "react"
+import React from 'react';
 
-import Layout from "../components/layout"
+import Layout from '../components/layout';
 
-import SEO from "../components/seo"
+import SEO from '../components/seo';
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Home</h1>
-  </Layout>
-)
+function IndexPage() {
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Home</h1>
+    </Layout>
+  );
+}
 
-export default IndexPage
+export default IndexPage;
