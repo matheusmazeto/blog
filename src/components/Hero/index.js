c9f30c9ee32679e86e55be13053b75c8999e362d
Matheus Mazeto <mgmazeto@gmail.com>
2020-01-15 23:50:14 -0300

    Add hero content

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 function Hero() {
-  return <div>hero</div>;
+  return (
+    <section>
+      {/* use playfair */}
+      <h1>Hi, my Name is Matheus Mazeto</h1>
+      <p>I'm a software developer currently living in Brazil.</p>
+      <p>
+        I have been working with web technologies, such as React, React Native
+        and NodeJS.
+      </p>
+      <p>
+        You can read my <Link to="/blog">Blog</Link> and also find my complete
+        professional background and experience on my{' '}
+        <a href="https://www.linkedin.com/in/matheusmazeto/">Linkedin</a>.
+      </p>
+    </section>
+  );
 }
 
 export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import Layout from '../components/Layout';
+import Hero from '../components/Hero';
 
 import SEO from '../components/seo';
 
@@ -8,7 +9,7 @@ function IndexPage() {
   return (
     <Layout>
       <SEO title="Home" />
-      <h1>Home</h1>
+      <Hero />
     </Layout>
   );
 }
