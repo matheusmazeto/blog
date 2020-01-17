b2bd7a06165afe6140b1439fc082e07752f62ef1
Matheus Mazeto <mgmazeto@gmail.com>
2020-01-17 00:16:02 -0300

    Add posts folder

--- src/components/PostItem/index.js
+++ src/components/PostItem/index.js
@@ -1,42 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import { Javascript } from 'styled-icons/boxicons-logos/Javascript';
-
 import * as S from './styled';
 
-function PostItem({
-  slug,
-  background,
-  category,
-  date,
-  timeToRead,
-  title,
-  description,
-}) {
+function PostItem({ slug, title, category, description, date, timeToRead }) {
   return (
-    <S.PostItemLink to="/slug/">
+    <S.PostItemLink to={slug}>
       <S.PostItemWrapper>
         <S.PostItemInfo>
           <S.WrapperPostItemTitle>
-            <S.PostItemTitle>
-              Diga não ao Medium: tenha sua própria plataforma
-            </S.PostItemTitle>
-            <S.PostItemTag background="#47650b">
-              <Javascript size="2.5rem" color="#000" />
-            </S.PostItemTag>
+            <S.PostItemTitle>{title}</S.PostItemTitle>
+            <S.PostItemTag>{category}</S.PostItemTag>
           </S.WrapperPostItemTitle>
-          <S.PostItemDescription>
-            Algumas razões para você ter sua própria plataforma ao invés de
-            soluções como o Medium.
-          </S.PostItemDescription>
+          <S.PostItemDescription>{description}</S.PostItemDescription>
           <S.PostItemDate>
-            30 de Julho de 2019 • 4 min de leitura
+            {date} • {timeToRead} min de leitura
           </S.PostItemDate>
         </S.PostItemInfo>
       </S.PostItemWrapper>
     </S.PostItemLink>
   );
 }
+
+PostItem.propTypes = {
+  slug: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  category: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  timeToRead: PropTypes.string.isRequired,
+};
 
 export default PostItem;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,14 +5,20 @@ import Layout from '../components/Layout';
 import Hero from '../components/Hero';
 import PostItem from '../components/PostItem';
 
+import { Javascript } from 'styled-icons/boxicons-logos/Javascript';
+
 function IndexPage() {
   return (
     <Layout>
       <SEO title="Home" />
-      {/* <Hero /> */}
-      <PostItem />
-      <PostItem />
-      <PostItem />
+      <PostItem
+        slug="/about/"
+        category={<Javascript size="25px" color="#000" />}
+        date="30 de Julho de 2019"
+        timeToRead="5"
+        title="Diga não ao Medium: tenha sua própria plataforma"
+        description="Algumas razões para você ter sua própria plataforma ao invés de soluções como o Medium."
+      />
     </Layout>
   );
 }
