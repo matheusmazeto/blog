e79626763ee46426b763cccdee87c2e87eb94f98
Matheus Mazeto <mgmazeto@gmail.com>
2020-01-18 16:30:37 -0300

    Add pagination to blog

--- gatsby-node.js
+++ gatsby-node.js
@@ -38,7 +38,9 @@ exports.createPages = ({ graphql, actions }) => {
       }
     }
   `).then((result) => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const posts = result.data.allMarkdownRemark.edges;
+
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve(`./src/templates/blog-post.js`),
@@ -46,6 +48,22 @@ exports.createPages = ({ graphql, actions }) => {
           // Data passed to context is available
           // in page queries as GraphQL variables.
           slug: node.fields.slug,
+        },
+      });
+    });
+
+    const postsPerPage = 3;
+    const numPages = Math.ceil(posts.length / postsPerPage);
+
+    Array.from({ length: numPages }).forEach((_, index) => {
+      createPage({
+        path: index === 0 ? `/` : `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1,
         },
       });
     });
